9d5097fe712cb52c5b6a2540df6e7a2e83e28269
Asher Dawes <asher.dawes@gmail.com>
2019-03-25 05:57:11 -0700

    Portable fix (#50)
    * Fix portable (#49)
    
    * Working with -O2 
    
    * Enabling debug symbols
    
    * Disabling CPU_XENONCAT on Cmake config
    
    * Enabling Mac builds

--- scripts/start.bat
+++ scripts/start.bat
@@ -7,6 +7,7 @@
 @call :GET_CURRENT_DIR
 @cd %THIS_DIR%
 nheqminer.exe -v -l %PoolHost%:%Port% -u %PublicVerusCoinAddress%.%WorkerName% -t %Threads%  %1 %2 %3 %4 %5 %6 %7 %8 %9
+pause
 @goto :EOF
 
 :GET_CURRENT_DIR
